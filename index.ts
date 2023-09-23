49474e8c1a33c1f11a1aaec12e2a79ac4fc92abd
Kokiat Tankoem <kokiat.t@tcc-technology.com>
2023-09-23 20:44:19 +0700

    simple bun webserver

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 5000,
+    fetch(req) {
+        return new Response('Hello BUN API')
+    }
+})
+
+console.log(`Listening on port ${server.port}`)
